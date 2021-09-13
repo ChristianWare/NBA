e1bad0a1a007b3c8e7f057d7eeb04afff7c2d7e0
Chris <chris.ware.dev@gmail.com>
2021-09-12 17:18:13 -0700

    getStaticProps config with mapping of Json data

--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,30 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
 
-export default function Home() {
+export default function Home({ teams }) {
+  console.log(teams);
+
   return (
-    <div>
+    <div className={styles.container}>
       <Head>
         <title>Next NBA</title>
         <link rel='icon' href='/favicon.io' />
       </Head>
       <main>
         <h1>NBA Teams</h1>
+        {teams.data.map((team) => (
+          <li key={team.id}>{team.name}</li>
+        ))}
       </main>
     </div>
   );
 }
 
-
 export async function getStaticProps(context) {
   const res = await fetch("https://www.balldontlie.io/api/v1/teams");
+  const teams = await res.json();
+
+  return {
+    props: { teams },
+  };
 }
